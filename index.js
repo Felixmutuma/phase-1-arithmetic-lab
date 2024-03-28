1bb01f342913b22330634958d44dbf68cebb1355
Felix Mutuma <felicmutuma8@gmail.com>
2024-03-28 18:00:12 +0300

    commits phase1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random= Math.floor(Math.random()+1);
+
+let num3=25;
+let num4=21;
+const mod = num3%num4;
+
+let numbers = [5, 10, 15, 20, 17]; // Example set of numbers
+let max = Math.max(...numbers);
